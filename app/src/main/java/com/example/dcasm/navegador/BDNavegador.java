6de9f6cea6991fef2e8c35473113b1e28bbbfcee
dcasmor <dcasmor@outlook.com>
2016-11-03 17:57:58 +0100

    Some things

---
+++ app/src/main/java/com/example/dcasm/navegador/BDNavegador.java
@@ -0,0 +1,21 @@
+package com.example.dcasm.navegador;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BDNavegador extends SQLiteOpenHelper {
+    public BDNavegador(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
