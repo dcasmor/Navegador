1fc0ff08f83f1e8b7d7ea25c903f8dadc1c1b182
dcasmor <dcasmor@outlook.com>
2016-12-17 15:50:31 +0100

    Stuff

--- app/src/main/java/com/example/dcasm/navegador/MainActivity.java
+++ app/src/main/java/com/example/dcasm/navegador/MainActivity.java
@@ -3,20 +3,22 @@ package com.example.dcasm.navegador;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     BrowserDB bd;
 
-    private WebView webview;
+    private WebView web;
     private AutoCompleteTextView direccion;
+    private ProgressBar pBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +26,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         bd = new BrowserDB(this);
 
         direccion = (AutoCompleteTextView) findViewById(R.id.acDireccion);
-        webview = (WebView) findViewById(R.id.webV);
-        webview.requestFocus();
+        web = (WebView) findViewById(R.id.webV);
+        pBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        webview.loadUrl("http://www.goole.es");
-        webview.setWebViewClient(new WebViewClient());
-        direccion.setText(webview.getUrl().toString());
+        direccion.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    web.loadUrl(direccion.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        pBar.setMax(100);
+        web.requestFocus();
+
+        web.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView webView, String url) {
+                direccion.setText(url);
+            }
+        });
+        web.loadUrl("http://www.google.es");
+        direccion.setText(web.getUrl().toString());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack() == true) {
+            web.goBack();
+        }
+        else
+            finish();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.menu.menu_main) {
+            web.loadUrl("http://www.google.es");
+        }
 
         return super.onOptionsItemSelected(item);
     }
